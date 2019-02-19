dcbc68055efcc852600158f9dba2b2362cb22e8e
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-02-19 21:57:05 +0100

    Add files via upload

---
+++ Day 02 Part 2/main.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+
+int main() {
+	std::ios::sync_with_stdio(false);
+
+	auto filename = std::string{"dimensions.txt"};
+	auto file = std::fstream{filename};
+
+	if(file.is_open()) {
+		auto dimensions = std::vector<std::string>{3};
+		auto total_ribbon = std::uint64_t{};
+
+		auto line = std::string{};
+		while(std::getline(file, line)) {
+			boost::split(dimensions, line, [] (char c) { return (c == 'x'); });
+
+			auto length = std::stoul(dimensions[0]);
+			auto width = std::stoul(dimensions[1]);
+			auto height = std::stoul(dimensions[2]);
+
+			// sorts 3 elements
+			if(width < length) { std::swap(length, width); }
+			if(height < width) {
+				std::swap(width, height);
+				if(width < length) { std::swap(length, width); }
+			}
+
+			// after sorting, length & wdith are now the two shortest sides
+			total_ribbon += 2 * (length + width) + (length * width * height);
+		}
+
+		std::cout << "Total ribbon: " << total_ribbon << std::endl;
+	} else {
+		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
+	}
+
+	return 0;
+}
