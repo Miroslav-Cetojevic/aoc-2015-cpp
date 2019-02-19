1b3762e059b547c486cda4f8f8b36244e6041a31
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-02-19 21:52:37 +0100

    Add files via upload

---
+++ Day 02 Part 1/main.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+
+int main() {
+	std::ios::sync_with_stdio(false);
+
+	auto filename = std::string{"dimensions.txt"};
+	auto file = std::fstream{filename};
+
+	if(file.is_open()) {
+		auto dimensions = std::vector<std::string>{3};
+		auto total_paper = std::uint64_t{};
+
+		auto line = std::string{};
+		while(std::getline(file, line)) {
+			boost::split(dimensions, line, [] (char c) { return (c == 'x'); });
+
+			auto length = std::stoul(dimensions[0]);
+			auto width = std::stoul(dimensions[1]);
+			auto height = std::stoul(dimensions[2]);
+
+			auto A = length * width;
+			auto B = width * height;
+			auto C = length * height;
+
+			total_paper += (2 * (A + B + C)) + std::min(std::min(A, B), C);
+		}
+		std::cout << total_paper << std::endl;
+	} else {
+		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
+	}
+
+	return 0;
+}
