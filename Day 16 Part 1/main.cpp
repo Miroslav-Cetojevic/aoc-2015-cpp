2b12bee7a159b7a0b8391e5f2b51f69ac625a290
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-02-23 20:07:41 +0100

    Add files via upload

--- Day 16 Part 1/main.cpp
+++ Day 16 Part 1/main.cpp
@@ -1,69 +1,72 @@
+#include <algorithm>
 #include <chrono>
-#include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <unordered_map>
+#include <map>
 #include <string>
 #include <vector>
 
 #include <boost/algorithm/string.hpp>
 
+#include "flat_hash_map.hpp"
+
 struct Sue {
 	std::size_t id;
-	std::unordered_map<std::string, std::size_t> detectables;
+	std::map<std::size_t, std::size_t> detectables;
 };
+
+template<typename T>
+auto get_hash(T t) { return std::hash<T>{}(t); }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-	std::string filename{"sue.txt"};
-	std::fstream file{filename};
+
+	auto filename = std::string{"sue.txt"};
+	auto file = std::fstream{filename};
+
 	if(file.is_open()) {
-		auto start = std::chrono::high_resolution_clock::now();
-		std::vector<Sue> aunties;
+		auto start = std::chrono::steady_clock::now();
+
+		auto aunties = std::vector<Sue>{};
+
+		auto tokens = std::vector<std::string>{};
+
 		std::string line;
 		while(std::getline(file, line)) {
-			Sue aunt;
-			std::vector<std::string> tokens;
-			boost::split(tokens, line, [] (const char c) {
-				return (c == ' ');
-			});
+
+			boost::split(tokens, line, [] (auto c) { return (c == ' '); });
+
+			auto aunt = Sue{};
 			aunt.id = std::stoul(tokens[0]);
-			for(std::size_t i = 2; i < tokens.size(); i += 2) {
-				aunt.detectables[tokens[i-1]] = std::stoul(tokens[i]);
+
+			for(auto i = 2UL; i < tokens.size(); i += 2) {
+				aunt.detectables[get_hash(tokens[i-1])] = std::stoul(tokens[i]);
 			}
-			aunties.push_back(aunt);
+
+			aunties.emplace_back(aunt);
 		}
-		// find the correct aunt
-		Sue sue{0, {{"children", 3},
-					{"cats", 7},
-					{"samoyeds", 2},
-					{"pomeranians", 3},
-					{"akitas", 0},
-					{"vizslas", 0},
-					{"goldfish", 5},
-					{"trees", 3},
-					{"cars", 2},
-					{"perfumes", 1}}};
-		std::vector<Sue> search_results;
-		for(const auto& aunt : aunties) {
-			bool is_potential_match = true;
-			for(const auto& detectable : sue.detectables) {
-				auto result = aunt.detectables.find(detectable.first);
-				if(result != aunt.detectables.end() && result->second != detectable.second) {
-					is_potential_match = false;
-					break;
-				}
-			}
-			if(is_potential_match) {
-				search_results.push_back(aunt);
-			}
-		}
-		auto end = std::chrono::high_resolution_clock::now();
-		std::cout << search_results.front().id << std::endl;
+
+		decltype(Sue::detectables) sue{{get_hash(std::string{"children"}), 3},
+									   {get_hash(std::string{"cats"}), 7},
+									   {get_hash(std::string{"samoyeds"}), 2},
+									   {get_hash(std::string{"pomeranians"}), 3},
+									   {get_hash(std::string{"akitas"}), 0},
+									   {get_hash(std::string{"vizslas"}), 0},
+									   {get_hash(std::string{"goldfish"}), 5},
+									   {get_hash(std::string{"trees"}), 3},
+									   {get_hash(std::string{"cars"}), 2},
+									   {get_hash(std::string{"perfumes"}), 1}};
+
+		auto result = std::find_if(aunties.begin(), aunties.end(), [&sue] (auto aunt) {
+			return std::includes(sue.begin(), sue.end(), aunt.detectables.begin(), aunt.detectables.end());
+		});
+
+		auto end = std::chrono::steady_clock::now();
+		std::cout << result->id << std::endl;
 		std::cout << "duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << std::endl;
 
 	} else {
-		std::cerr << "Error! Could not open " << filename << "!" << std::endl;
+		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
 
 	return 0;
