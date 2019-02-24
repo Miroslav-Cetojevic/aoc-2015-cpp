d8d303fe0bda2312fcf2684d81b0304ff166e800
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-02-24 16:48:44 +0100

    Add files via upload

--- Day 16 Part 1/main.cpp
+++ Day 16 Part 1/main.cpp
@@ -26,7 +26,7 @@ auto get_hash(T t) { return std::hash<T>{}(t); }
 int main() {
 	std::ios_base::sync_with_stdio(false);
 
-	auto filename = std::string{"sue.txt"};
+	auto filename = std::string{"aunts.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
@@ -54,16 +54,18 @@ int main() {
 			aunties.emplace_back(aunt);
 		}
 
-		decltype(Sue::detectables) sue{{get_hash(std::string{"children"}), 3},
-									   {get_hash(std::string{"cats"}), 7},
-									   {get_hash(std::string{"samoyeds"}), 2},
-									   {get_hash(std::string{"pomeranians"}), 3},
-									   {get_hash(std::string{"akitas"}), 0},
-									   {get_hash(std::string{"vizslas"}), 0},
-									   {get_hash(std::string{"goldfish"}), 5},
-									   {get_hash(std::string{"trees"}), 3},
-									   {get_hash(std::string{"cars"}), 2},
-									   {get_hash(std::string{"perfumes"}), 1}};
+		decltype(Sue::detectables) sue{
+			{get_hash(std::string{"children"}), 3},
+			{get_hash(std::string{"cats"}), 7},
+			{get_hash(std::string{"samoyeds"}), 2},
+			{get_hash(std::string{"pomeranians"}), 3},
+			{get_hash(std::string{"akitas"}), 0},
+			{get_hash(std::string{"vizslas"}), 0},
+			{get_hash(std::string{"goldfish"}), 5},
+			{get_hash(std::string{"trees"}), 3},
+			{get_hash(std::string{"cars"}), 2},
+			{get_hash(std::string{"perfumes"}), 1}
+		};
 
 		auto result = std::find_if(aunties.begin(), aunties.end(), [&sue] (auto aunt) {
 			return std::includes(sue.begin(), sue.end(), aunt.detectables.begin(), aunt.detectables.end());
