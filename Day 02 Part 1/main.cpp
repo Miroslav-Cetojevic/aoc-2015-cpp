9e296868053721bb04489ea241455c93e1b174ed
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-17 19:32:08 +0100

    Improved readability
    Minor changes to improve overall readability.

--- Day 02 Part 1/main.cpp
+++ Day 02 Part 1/main.cpp
@@ -3,32 +3,36 @@
 #include <iostream>
 #include <string>
 
-struct Dimensions { std::size_t length, width, height; };
+struct Dimensions {
+	std::size_t length, width, height;
+};
 
 auto& operator>>(std::istream& in, Dimensions& D) {
 	return in >> D.length >> D.width >> D.height;
 }
 
 int main() {
-	std::ios::sync_with_stdio(false);
 
 	auto filename = std::string{"dimensions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
-		auto total_paper = 0UL;
+
+		auto total_paper = 0U;
 
 		Dimensions D;
+
 		while(file >> D) {
 
 			auto A = D.length * D.width;
 			auto B = D.width * D.height;
 			auto C = D.length * D.height;
 
-			total_paper += (2 * (A + B + C)) + std::min(std::min(A, B), C);
+			total_paper += (2 * (A + B + C)) + std::min({A, B, C});
 		}
 
 		std::cout << total_paper << std::endl;
+
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
