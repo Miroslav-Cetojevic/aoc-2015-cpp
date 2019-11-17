ff4931cb1a924c60b803ebede4ba6ee932257d51
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-17 22:51:48 +0100

    Improved readability
    Instead of sorting 3 elements I decided to simply compute the 2 smallest elements. Some minor changes, too.

--- Day 02 Part 2/main.cpp
+++ Day 02 Part 2/main.cpp
@@ -1,42 +1,45 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <utility>
 
-struct Dimensions { std::size_t length, width, height; };
+struct Dimensions {
+	unsigned length, width, height;
+};
 
 auto& operator>>(std::istream& in, Dimensions& D) {
 	return in >> D.length >> D.width >> D.height;
 }
 
 int main() {
-	std::ios::sync_with_stdio(false);
 
 	auto filename = std::string{"dimensions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
-		auto total_ribbon = 0UL;
+
+		auto total_ribbon = 0U;
 
 		Dimensions D;
+
 		while(file >> D) {
 
-			auto length = D.length;
-			auto width = D.width;
-			auto height = D.height;
+			auto& l = D.length;
+			auto& w = D.width;
+			auto& h = D.height;
 
-			// sorts 3 elements
-			if(width < length) { std::swap(length, width); }
-			if(height < width) {
-				std::swap(width, height);
-				if(width < length) { std::swap(length, width); }
-			}
+			// get the two smallest sides, min1 & min2
+			if(l > w) { std::swap(l, w); }
 
-			// after sorting, length & wdith are now the two shortest sides
-			total_ribbon += 2 * (length + width) + (length * width * height);
+			auto min1 = l;
+			auto min2 = std::min(w, h);
+
+			total_ribbon += 2 * (min1 + min2) + (l * w * h);
 		}
 
 		std::cout << total_ribbon << std::endl;
+
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
