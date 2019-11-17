eb634e5b5de3f7df80a6e0c67db6de82847dbe21
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-17 19:18:57 +0100

    Improved readability
    Similar to Part 1, additionally I replaced `for` with `while`.

--- Day 01 Part 2/main.cpp
+++ Day 01 Part 2/main.cpp
@@ -3,21 +3,30 @@
 #include <string>
 
 int main() {
-	std::ios::sync_with_stdio(false);
 
 	auto filename = std::string{"directions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
-		auto floor = 0L;
-		auto pos = 0UL;
 
-		char direction;
-		for(auto& i = pos; (floor >= 0) && (file >> direction); ++i) {
-			floor += ((direction == '(') ? 1 : -1);
+		auto pos = 0U;
+		auto floor = 0;
+		auto UP = '(';
+		decltype(UP) direction;
+
+		while((floor >= 0) && (file >> direction)) {
+
+			if(direction == UP) {
+				++floor;
+			} else {
+				--floor;
+			}
+
+			++pos;
 		}
 
 		std::cout << pos << std::endl;
+
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
