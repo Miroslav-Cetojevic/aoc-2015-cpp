c4c57c6fa905beefbff356248f8dca0af79250ae
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-17 19:04:03 +0100

    Improve readability
    Replaced the ternary operator with an if-else clause, plus other minor changes.

--- Day 01 Part 1/main.cpp
+++ Day 01 Part 1/main.cpp
@@ -3,22 +3,29 @@
 #include <string>
 
 int main() {
-	std::ios::sync_with_stdio(false);
 
 	auto filename = std::string{"directions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
-		auto floor = 0L;
 
-		char direction;
+		auto floor = 0;
+		auto UP = '(';
+		decltype(UP) direction;
+
 		while(file >> direction) {
-			floor += ((direction == '(') ? 1 : -1);
+
+			if(direction == UP) {
+				++floor;
+			} else {
+				--floor;
+			}
 		}
 
 		std::cout << floor << std::endl;
+
 	} else {
-		std::cerr << "Error! Could not open \"" << filename << "\"" << std::endl;
+		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
 
 	return 0;
