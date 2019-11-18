59f3e8ea5f93c9d32cf99a537b59b2f4e7ae5cec
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-18 22:04:09 +0100

    Basically the same changes as Part 1

--- Day 04 Part 2/main.cpp
+++ Day 04 Part 2/main.cpp
@@ -1,15 +1,25 @@
 /*
- * NOTE: this code uses the poco library and is compiled with the -lPocoFoundation flag
+ * NOTE:
+ *
+ * This code uses Boost and Poco C++
+ *
+ * I'm using the following flags:
+ * Compiler: -I/usr/include/Poco
+ * Linker: -L/usr/lib -lPocoFoundation
+ *
+ * !! It takes a couple seconds to compute the result !!
+ *
  */
 #include <algorithm>
 #include <iostream>
+#include <limits>
 #include <string>
 
 #include <boost/iterator/counting_iterator.hpp>
 
-#include <Poco/MD5Engine.h>
 #include <Poco/DigestEngine.h>
 #include <Poco/DigestStream.h>
+#include <Poco/MD5Engine.h>
 
 template<typename T>
 struct Range {
@@ -18,17 +28,31 @@ struct Range {
 };
 
 int main() {
+
 	auto key = std::string{"ckczppom"};
 	auto prefix = std::string{"000000"};
-	auto old_result = 117946UL;
 
 	auto md5 = Poco::MD5Engine{};
 	auto out = Poco::DigestOutputStream{md5};
 
-	auto range = Range{(old_result + 1), std::numeric_limits<std::size_t>::max()};
+	// avoid repeating computations from Part 1
+	auto start = 117946U + 1;
+	auto finish = std::numeric_limits<unsigned>::max();
+
+	auto range = Range{start, finish};
+
 	auto result = std::find_if(range.begin, range.end, [&] (auto i) {
-		(out << key + std::to_string(i)).flush();
-		return (Poco::DigestEngine::digestToHex(md5.digest()).compare(0, prefix.size(), prefix) == 0);
+
+		// computes the digest of (key + i) to be retrieved by the engine later
+		out << (key + std::to_string(i));
+		out.close();
+
+		// finishes retrieve the hash in hexadecimal form as a string
+		auto value = Poco::DigestEngine::digestToHex(md5.digest());
+
+		auto has_prefix = (value.compare(0, prefix.size(), prefix) == 0);
+
+		return has_prefix;
 	});
 
 	std::cout << *result << std::endl;
