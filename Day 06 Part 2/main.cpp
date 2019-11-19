05fc990e3930f480441432af4c8188d328a0503b
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-19 17:32:17 +0100

    Changes are similar to Part 1

--- Day 06 Part 2/main.cpp
+++ Day 06 Part 2/main.cpp
@@ -1,44 +1,43 @@
-#include <array>
+#include <bitset>
 #include <fstream>
 #include <iostream>
-#include <numeric>
 #include <string>
 #include <unordered_map>
 
-struct Position { std::size_t row, col; };
+struct Position {
+	unsigned row, col;
+};
 
 auto& operator>>(std::istream& in, Position& pos) {
 	return in >> pos.row >> pos.col;
 }
 
-struct EndPoints { Position first, last; };
+struct EndPoints {
+	Position first, last;
+};
 
 auto& operator>>(std::istream& in, EndPoints& pos) {
 	return in >> pos.first >> pos.last;
 }
 
-template<typename G, typename T>
-auto on(G& grid, T pos) { ++grid[pos]; }
-
-template<typename G, typename T>
-auto off(G& grid, T pos) { if(grid[pos] > 0) { --grid[pos]; } }
-
-template<typename G, typename T>
-auto toggle(G& grid, T pos) { grid[pos] += 2; }
-
 int main() {
-	std::ios_base::sync_with_stdio(false);
 
 	auto filename = std::string{"instructions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
-		constexpr auto gridlen = 1000UL;
+
+		constexpr auto gridlen = 1000U;
 		constexpr auto numlights = (gridlen * gridlen);
-		using Grid = std::array<std::size_t, numlights>;
+
+		using Grid = std::bitset<numlights>;
 		auto grid = Grid{};
 
-		using cmd = void (*) (Grid&, std::size_t);
+		auto on = [] (auto& grid, auto pos) { grid.set(pos); };
+		auto off = [] (auto& grid, auto pos) { grid.reset(pos); };
+		auto toggle = [] (auto& grid, auto pos) { grid.flip(pos); };
+
+		using cmd = void (*) (Grid&, unsigned);
 		auto cmd_map = std::unordered_map<std::string, cmd>{
 			{"on", on},
 			{"off", off},
@@ -50,20 +49,22 @@ int main() {
 
 		while(file >> instruction >> endpoints) {
 
-			auto command = cmd_map[instruction];
+			auto current_cmd = cmd_map[instruction];
 
 			for(auto row = endpoints.first.row; row <= endpoints.last.row; ++row) {
+
 				for(auto col = endpoints.first.col; col <= endpoints.last.col; ++col) {
-					command(grid, row*gridlen+col);
+
+					current_cmd(grid, (row * gridlen + col));
 				}
 			}
 		}
 
-		auto sum = std::accumulate(grid.begin(), grid.end(), 0UL);
+		std::cout << grid.count() << std::endl;
 
-		std::cout << sum << std::endl;
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
+
 	return 0;
 }
