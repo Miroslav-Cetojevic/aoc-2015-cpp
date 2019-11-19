da0fd05165e99d112a8c3e3a4aa830df3e6a6004
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-19 15:59:33 +0100

    Just a couple cosmetic changes

--- Day 06 Part 1/main.cpp
+++ Day 06 Part 1/main.cpp
@@ -12,13 +12,17 @@ struct Range {
 	Range(T b, T e): begin(b), end(e) {}
 };
 
-struct Position { std::size_t row, col; };
+struct Position {
+	unsigned row, col;
+};
 
 auto& operator>>(std::istream& in, Position& pos) {
 	return in >> pos.row >> pos.col;
 }
 
-struct EndPoints { Position first, last; };
+struct EndPoints {
+	Position first, last;
+};
 
 auto& operator>>(std::istream& in, EndPoints& pos) {
 	return in >> pos.first >> pos.last;
@@ -34,18 +38,19 @@ template<typename G, typename T>
 auto toggle(G& grid, T pos) { grid.flip(pos); }
 
 int main() {
-	std::ios_base::sync_with_stdio(false);
 
 	auto filename = std::string{"instructions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
-		constexpr auto gridlen = 1000UL;
+
+		constexpr auto gridlen = 1000U;
 		constexpr auto numlights = (gridlen * gridlen);
+
 		using Grid = std::bitset<numlights>;
 		auto grid = Grid{};
 
-		using cmd = void (*) (Grid&, std::size_t);
+		using cmd = void (*) (Grid&, unsigned);
 		auto cmd_map = std::unordered_map<std::string, cmd>{
 			{"on", on},
 			{"off", off},
@@ -57,16 +62,19 @@ int main() {
 
 		while(file >> instruction >> endpoints) {
 
-			auto command = cmd_map[instruction];
+			auto current_cmd = cmd_map[instruction];
 
 			for(auto row = endpoints.first.row; row <= endpoints.last.row; ++row) {
+
 				for(auto col = endpoints.first.col; col <= endpoints.last.col; ++col) {
-					command(grid, row*gridlen+col);
+
+					current_cmd(grid, (row * gridlen + col));
 				}
 			}
 		}
 
 		std::cout << grid.count() << std::endl;
+
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
