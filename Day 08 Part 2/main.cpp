2725ba9b4611f683ff6fa1472d0d69a2c541b53a
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-27 15:11:02 +0100

    A few cosmetic changes

--- Day 08 Part 2/main.cpp
+++ Day 08 Part 2/main.cpp
@@ -3,23 +3,23 @@
 #include <string>
 
 int main() {
-	std::ios_base::sync_with_stdio(false);
 
 	auto filename = std::string{"list.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
 
-		file.seekg(0, std::ios::end);
-		auto contents = std::string{};
-		contents.resize(file.tellg());
-		file.seekg(0, std::ios::beg);
-		file.read(contents.data(), contents.size());
+		auto list = std::string{};
 
-		auto encsum = std::size_t{};
+		file.seekg(0, std::ios::end);
+		list.resize(file.tellg());
+		file.seekg(0, std::ios::beg);
+		file.read(list.data(), list.size());
+
+		auto encsum = unsigned{};
 		auto nlsum = encsum;
 
-		for(auto it = contents.begin(); it != contents.end(); ++it) {
+		for(auto it = list.begin(); it != list.end(); ++it) {
 			auto c = *it;
 			if(c == '\n') { ++nlsum; }
 			if(c == '\\') {
@@ -35,11 +35,12 @@ int main() {
 			}
 		}
 
-		auto codesum = contents.size() - nlsum;
+		auto codesum = list.size() - nlsum;
 		auto new_codesum = codesum + (4 * nlsum) + encsum;
 		auto result = (new_codesum - codesum);
 
 		std::cout << result << std::endl;
+
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
