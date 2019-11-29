32085ad49b8262d280c05a723ea97916bda87a8e
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-11-29 17:03:14 +0100

    Same changes as Part 1

--- Day 11 Part 2/main.cpp
+++ Day 11 Part 2/main.cpp
@@ -2,71 +2,86 @@
 #include <functional>
 #include <iostream>
 #include <string>
+#include <string_view>
 
 #include <boost/iterator/counting_iterator.hpp>
 
 template<typename T>
-struct Range {
-	boost::counting_iterator<T, boost::use_default, T> begin, end;
-	Range(T b, T e): begin(b), end(e) {}
+class Range {
+	private:
+		boost::counting_iterator<T, boost::use_default, T> begin_, end_;
+	public:
+		Range(T b, T e): begin_(b), end_(e) {}
+		auto begin() const { return begin_; }
+		auto end() const { return end_; }
 };
 
-template<typename A, typename C>
-constexpr auto is_incremented(const A& alphabet, C& value) {
-	auto begin = alphabet.begin();
-	auto end = alphabet.end();
+template<typename T>
+class Reverse {
+	private:
+		T* ptr;
+	public:
+		Reverse(T& container): ptr(&container) {}
+		auto begin() const { return ptr->rbegin(); }
+		auto end() const { return ptr->rend(); }
+};
 
-	auto pos = std::find_if(begin, end, [&value] (auto c) {
-		return (c == value);
-	});
+template<typename P, typename A>
+constexpr auto increment_pwd(P& pwd, const A& alphabet) {
 
-	auto has_next = true;
-	if(pos == end || ++pos == end) {
-		value = *begin;
-		has_next = false;
-	} else {
-		value = *pos;
+	// todo: for_each(begin(v), find_if(begin(v), end(v), cond), f);?
+	for(auto& c : Reverse{pwd}) {
+
+		if(c != alphabet.back()) {
+			const auto pos = std::find(alphabet.begin(), alphabet.end(), c);
+			c = *pos + 1;
+			break;
+		}
+
+		c = alphabet.front();
 	}
-
-	return has_next;
 }
 
-template <typename P>
-constexpr auto incrementable(P& pwd) {
-	auto alphabet = std::string_view{"abcdefghijklmnopqrstuvwxyz"};
+template<typename P, typename A>
+constexpr auto is_incrementable(const P& pwd, const A& alphabet) {
 
-	auto count = std::count_if(pwd.begin(), pwd.end(), [&alphabet] (const auto letter) {
+	const auto count = std::count_if(pwd.begin(), pwd.end(), [&alphabet] (const auto letter) {
 		return (letter == alphabet.back());
 	});
 
-	auto is_incrementable = (static_cast<std::size_t>(count) != pwd.size());
+	const auto size = pwd.size();
 
-	if(is_incrementable) {
-		std::any_of(pwd.rbegin(), pwd.rend(), [&alphabet] (auto& letter) {
-			return is_incremented(alphabet, letter);
-		});
-	}
-	return is_incrementable;
+	return (static_cast<decltype(size)>(count) != size);
 }
 
 template<typename P>
 constexpr auto has_two_pairs(const P& pwd) {
-	auto first_pair = std::adjacent_find(pwd.begin(), pwd.end());
-	return (first_pair != pwd.end())
-		   && (std::adjacent_find((first_pair + 2), pwd.end()) != pwd.end());
+
+	const auto first_result = std::adjacent_find(pwd.begin(), pwd.end());
+	const auto has_one_pair = (first_result != pwd.end());
+
+	const auto second_result = std::adjacent_find((first_result + 2), pwd.end());
+	const auto has_another_pair = (second_result != pwd.end());
+
+	return has_one_pair && has_another_pair;
 }
 
 template<typename P>
 constexpr auto has_three_straight(const P& pwd) {
-	auto range = Range<std::size_t>{2, pwd.size()};
-	return std::any_of(range.begin, range.end, [&pwd] (auto i) {
+
+	auto pwdlen = pwd.length();
+
+	auto range = Range<decltype(pwdlen)>{2, pwdlen};
+
+	return std::any_of(range.begin(), range.end(), [&pwd] (auto i) {
 		return ((pwd[i]-1) == pwd[i-1]) && ((pwd[i]-2) == pwd[i-2]);
 	});
 }
 
 template<typename P>
 constexpr auto has_banned_letter(const P& pwd) {
-	auto banned_letter = std::find_if(pwd.begin(), pwd.end(), [] (char c) {
+
+	const auto banned_letter = std::find_if(pwd.begin(), pwd.end(), [] (auto c) {
 		return c == 'i' || c == 'o' || c == 'l';
 	});
 
@@ -76,18 +91,26 @@ constexpr auto has_banned_letter(const P& pwd) {
 template<typename P>
 constexpr auto next_pwd(P& pwd) {
 
-	while(incrementable(pwd)) {
+	const auto alphabet = std::string_view{"abcdefghijklmnopqrstuvwxyz"};
 
-		if(has_banned_letter(pwd)) { continue; }
+	auto not_secure = true;
 
-		if(has_two_pairs(pwd) && has_three_straight(pwd)) { break; }
+	while(not_secure && is_incrementable(pwd, alphabet)) {
+
+		increment_pwd(pwd, alphabet);
+
+		not_secure = (has_banned_letter(pwd) || !(has_two_pairs(pwd) && has_three_straight(pwd)));
+
 	}
 }
 
 int main() {
+
 	auto pwd = std::string{"cqjxjnds"};
+
 	next_pwd(pwd);
 	next_pwd(pwd);
+
 	std::cout << pwd << std::endl;
 
 	return 0;
