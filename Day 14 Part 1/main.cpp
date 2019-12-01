81d27bbf8e5ebebbb6a1e905872a57de7bd4d9f7
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-01 22:44:51 +0100

    Replaced while(>>) with istream_iterators...
    That way I could read from file and do computations in a single pass. Within a STL algorithm, no less.

--- Day 14 Part 1/main.cpp
+++ Day 14 Part 1/main.cpp
@@ -1,13 +1,15 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <numeric>
 #include <string>
-#include <vector>
 
 struct Reindeer {
 	std::string name;
-	std::size_t speed, runtime, resttime;
+	unsigned speed;
+	unsigned runtime;
+	unsigned resttime;
 };
 
 auto& operator>>(std::istream& in, Reindeer& r) {
@@ -15,36 +17,48 @@ auto& operator>>(std::istream& in, Reindeer& r) {
 }
 
 int main() {
-	std::ios_base::sync_with_stdio(false);
 
-	auto filename = std::string{"reindeers.txt"};
+	const auto filename = std::string{"reindeers.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
-		auto reindeers = std::vector<Reindeer>{};
 
-		std::size_t seconds;
+		unsigned seconds;
+
 		file >> seconds;
 
-		Reindeer entry;
-		while(file >> entry) {
-			reindeers.emplace_back(entry);
-		}
+		auto begin = std::istream_iterator<Reindeer>{file};
+		auto end   = std::istream_iterator<Reindeer>{};
+		auto init  = unsigned{};
+		auto max   = [seconds] (const auto current_max, const auto& reindeer) {
 
-		auto max_distance = std::accumulate(reindeers.begin(), reindeers.end(), 0UL, [seconds] (auto current_max, auto& reindeer) {
-			const auto distance = (reindeer.speed * reindeer.runtime);
-			const auto cycle = (reindeer.runtime + reindeer.resttime);
+			const auto runtime = reindeer.runtime;
+			const auto speed = reindeer.speed;
+
+			const auto distance = (speed * runtime);
+			const auto cycle = (runtime + reindeer.resttime);
 			const auto cycles = (seconds / cycle);
 			const auto time_left = (seconds % cycle);
 
 			auto tmp_distance = (distance * cycles);
 
-			tmp_distance += (time_left >= reindeer.runtime) ? distance : (reindeer.speed * time_left);
+			// has this reindeer enough time for another full sprint before resting?
+			const auto can_do_full_sprint = (time_left >= runtime);
+
+			if(can_do_full_sprint) {
+				tmp_distance += distance;
+			} else {
+				tmp_distance += (speed * time_left);
+			}
 
 			return std::max(current_max, tmp_distance);
-		});
+		};
+
+		// std::accumulate can be used to compute the minmax or a sequence
+		const auto max_distance = std::accumulate(begin, end, init, max);
 
 		std::cout << max_distance << std::endl;
+
 	} else {
 		std::cerr << "Error! Could not open " << filename << "!" << std::endl;
 	}
