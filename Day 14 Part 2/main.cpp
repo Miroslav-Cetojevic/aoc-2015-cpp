a052cf4c295d8602403ed8406ad70ac839287c61
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-01 23:00:48 +0100

    Extracted two functions from main()
    No istream_iterator this time, but I did get to use the range-for with just integers, thanks to Boost.

--- Day 14 Part 2/main.cpp
+++ Day 14 Part 2/main.cpp
@@ -8,70 +8,104 @@
 #include <boost/iterator/counting_iterator.hpp>
 
 template<typename T>
-struct Range {
-	boost::counting_iterator<T, boost::use_default, T> begin, end;
-	Range(T b, T e): begin(b), end(e) {}
+class Range {
+	private:
+		boost::counting_iterator<T, boost::use_default, T> begin_, end_;
+	public:
+		Range(T b, T e): begin_(b), end_(e) {}
+		auto begin() const { return begin_; }
+		auto end() const { return end_; }
 };
 
 struct Reindeer {
 	std::string name;
-	std::size_t speed, runtime, resttime, total_distance, score;
+	unsigned speed;
+	unsigned runtime;
+	unsigned resttime;
+	unsigned total_distance;
+	unsigned score;
 };
 
 auto& operator>>(std::istream& in, Reindeer& r) {
-	return in >> r.name >> r.speed >> r.runtime >> r.resttime;
+	in >> r.name >> r.speed >> r.runtime >> r.resttime;
+	r.total_distance = r.score = 0;
+	return in;
 }
 
 auto operator>(const Reindeer& lhs, const Reindeer& rhs) {
 	return (lhs.total_distance > rhs.total_distance);
 }
 
-int main() {
-	std::ios_base::sync_with_stdio(false);
+template<typename R, typename N>
+auto calculate_scores(R& reindeers, N seconds) {
 
-	auto filename = std::string{"reindeers.txt"};
+	auto timespan = Range<unsigned>{1, (seconds + 1)};
+
+	for(const auto second : timespan) {
+
+		for(auto& reindeer : reindeers) {
+
+			const auto runtime = reindeer.runtime;
+			const auto cycle = (runtime + reindeer.resttime);
+			const auto time_left = (second % cycle);
+
+			// check if reindeer is running right this second
+			const auto has_time_left = (time_left > 0) && (time_left <= runtime);
+
+			if(has_time_left) {
+				reindeer.total_distance += reindeer.speed;
+			}
+		}
+
+		// update the ranking in total distance
+		std::sort(reindeers.begin(), reindeers.end(), std::greater<Reindeer>{});
+
+		// one point for each leader
+		for(auto& reindeer : reindeers) {
+			if(reindeer.total_distance == reindeers.front().total_distance) {
+				++(reindeer.score);
+			}
+		}
+	}
+}
+
+template<typename R>
+auto get_max_score(const R& reindeers) {
+
+	const auto begin = reindeers.begin();
+	const auto end   = reindeers.end();
+	const auto init  = unsigned{};
+	const auto max   = [] (auto current_max, auto& reindeer) {
+		return std::max(current_max, reindeer.score);
+	};
+
+	// std::accumulate can be used to compute the minmax or a sequence
+	return std::accumulate(begin, end, init, max);
+}
+
+int main() {
+
+	const auto filename = std::string{"reindeers.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
+
 		auto reindeers = std::vector<Reindeer>{};
 
-		std::size_t seconds;
+		unsigned seconds;
+
 		file >> seconds;
 
 		Reindeer entry;
+
 		while(file >> entry) {
-			entry.total_distance = entry.score = 0;
 			reindeers.emplace_back(entry);
 		}
 
-		auto range = Range<std::size_t>{1, (seconds + 1)};
-		std::for_each(range.begin, range.end, [&reindeers] (auto i) {
+		calculate_scores(reindeers, seconds);
 
-			for(auto& reindeer : reindeers) {
-				const auto cycle = (reindeer.runtime + reindeer.resttime);
-				const auto time_left = (i % cycle);
+		std::cout << get_max_score(reindeers) << std::endl;
 
-				const auto has_time_left = (time_left > 0 && time_left <= reindeer.runtime);
-
-				if(has_time_left) {
-					reindeer.total_distance += reindeer.speed;
-				}
-			}
-
-			std::sort(reindeers.begin(), reindeers.end(), std::greater<Reindeer>{});
-
-			for(auto& reindeer : reindeers) {
-				if(reindeer.total_distance == reindeers.front().total_distance) {
-					++(reindeer.score);
-				}
-			}
-		});
-
-		auto max_score = std::accumulate(reindeers.begin(), reindeers.end(), 0UL, [] (auto current_max, auto& reindeer) {
-			return std::max(current_max, reindeer.score);
-		});
-
-		std::cout << max_score << std::endl;
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
 	}
