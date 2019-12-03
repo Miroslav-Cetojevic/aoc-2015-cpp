21c510f19e03fc45c520bad7b25164db1215e24d
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-03 13:54:05 +0100

    Extracted the "find the real Sue" code to a function...
    Plus some refactoring.

--- Day 16 Part 2/main.cpp
+++ Day 16 Part 2/main.cpp
@@ -9,51 +9,117 @@
 #include <unordered_map>
 #include <vector>
 
+// std::include is used later in the code, which requires a sorted container,
+// and I found std::map to be a very convenient tool for it
+using Clues = std::map<std::string, unsigned>;
+
 struct AuntSue {
-	std::size_t id;
-	std::map<std::size_t, std::size_t> detectables;
+	unsigned id;
+	Clues clues;
 };
 
-struct Detectable {
-	std::size_t amount;
+struct Clue {
 	std::string name;
+	unsigned amount;
 };
 
-auto& operator>>(std::istream& in, Detectable& detectable) {
-	return in >> detectable.name >> detectable.amount;
+auto& operator>>(std::istream& in, Clue& clue) {
+	return in >> clue.name >> clue.amount;
 }
 
-template<typename T>
-auto get_hash(T t) { return std::hash<T>{}(t); }
+auto extract_items_of_interest(Clues& sue, const Clues& differences) {
 
-struct Items {
-	std::size_t children = get_hash(std::string{"children"});
-	std::size_t cats = get_hash(std::string{"cats"});
-	std::size_t samoyeds = get_hash(std::string{"samoyeds"});
-	std::size_t pomeranians = get_hash(std::string{"pomeranians"});
-	std::size_t akitas = get_hash(std::string{"akitas"});
-	std::size_t vizslas = get_hash(std::string{"vizslas"});
-	std::size_t goldfish = get_hash(std::string{"goldfish"});
-	std::size_t trees = get_hash(std::string{"trees"});
-	std::size_t cars = get_hash(std::string{"cars"});
-	std::size_t perfumes = get_hash(std::string{"perfumes"});
-};
-
-template<typename D>
-auto extract_items_of_interest(const D& differences, D& detectables) {
-	auto items_of_interest = D{};
+	auto items_of_interest = Clues{};
 
 	for(const auto& difference : differences) {
-		items_of_interest.insert(detectables.extract(difference.first));
+		items_of_interest.insert(sue.extract(difference.first));
 	}
 
 	return items_of_interest;
 }
 
-int main() {
-	std::ios_base::sync_with_stdio(false);
+auto find_real_sue(const std::vector<AuntSue>& aunties) {
 
-	auto filename = std::string{"aunts.txt"};
+	static const auto CHILDREN = std::string{"children"};
+	static const auto CATS = std::string{"cats"};
+	static const auto SAMOYEDS = std::string{"samoyeds"};
+	static const auto POMERANIANS = std::string{"pomeranians"};
+	static const auto AKITAS = std::string{"akitas"};
+	static const auto VIZSLAS = std::string{"vizslas"};
+	static const auto GOLDFISH = std::string{"goldfish"};
+	static const auto TREES = std::string{"trees"};
+	static const auto CARS = std::string{"cars"};
+	static const auto PERFUMES = std::string{"perfumes"};
+
+	static const auto sue = Clues{
+		{CHILDREN, 3},
+		{SAMOYEDS, 2},
+		{AKITAS, 0},
+		{VIZSLAS, 0},
+		{CARS, 2},
+		{PERFUMES, 1}
+	};
+
+	static const auto differences = Clues{
+		{CATS, 7},
+		{POMERANIANS, 3},
+		{GOLDFISH, 5},
+		{TREES, 3},
+	};
+
+	// need std::function to make use of std::greater and std::less
+	using cmp = std::function<bool(unsigned, unsigned)>;
+
+	static auto cmp_map = std::unordered_map<std::string, cmp>{
+		{CATS, std::greater{}},
+		{TREES, std::greater{}},
+		{POMERANIANS, std::less{}},
+		{GOLDFISH, std::less{}}
+	};
+
+	auto real_sue = std::find_if(aunties.begin(), aunties.end(), [] (auto& aunt) {
+
+		auto other_sue = aunt.clues;
+
+		// in order to compare "sue" with "other sue", first we need to extract
+		// the items we know "sue" has, but their amounts are uncertain
+		auto items_of_interest = extract_items_of_interest(other_sue, differences);
+
+		auto is_subset = std::includes(sue.begin(), sue.end(), other_sue.begin(), other_sue.end());
+
+		const auto begin = items_of_interest.begin();
+		const auto end   = items_of_interest.end();
+		const auto init  = is_subset;
+		const auto op    = [] (auto acc, auto& item_of_interest) {
+
+			const auto item = differences.find(item_of_interest.first);
+
+			const auto found_item = (item != differences.end());
+
+			const auto key = item->first;
+			const auto comparator = item->second;
+			const auto amount = item_of_interest.second;
+
+			const auto result = (found_item ? cmp_map[key](amount, comparator) : true);
+
+			return (acc && result);
+		};
+
+		// is_found can only be true if all items of interest have the differences we are looking for
+		auto is_found = std::accumulate(begin, end, init, op);
+
+		return is_found;
+	});
+
+	// if the input file wasn't guaranteed to have the result we are looking for,
+	// a comparison with the end-iterator would have been necessary before accessing
+	// the object, but since this is AoC, we do have the (implicit) guarantee
+	return real_sue->id;
+}
+
+int main() {
+
+	const auto filename = std::string{"aunts.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
@@ -63,6 +129,7 @@ int main() {
 		auto ss = std::stringstream{};
 
 		std::string line;
+
 		while(std::getline(file, line)) {
 
 			ss.str(line);
@@ -71,61 +138,18 @@ int main() {
 
 			ss >> aunt.id;
 
-			Detectable detectable;
-			while(ss >> detectable) {
-				aunt.detectables[get_hash(detectable.name)] = detectable.amount;
+			Clue clue;
+
+			while(ss >> clue) {
+				aunt.clues[clue.name] = clue.amount;
 			}
 
-			ss.clear();
+			aunties.push_back(aunt);
 
-			aunties.emplace_back(aunt);
+			ss.clear();
 		}
 
-		auto items = Items{};
-
-		decltype(AuntSue::detectables) sue{
-			{items.children, 3},
-			{items.samoyeds, 2},
-			{items.akitas, 0},
-			{items.vizslas, 0},
-			{items.cars, 2},
-			{items.perfumes, 1}
-		};
-
-		decltype(AuntSue::detectables) differences{
-			{items.cats, 7},
-			{items.pomeranians, 3},
-			{items.goldfish, 5},
-			{items.trees, 3},
-		};
-
-		using cmp = std::function<bool(const std::size_t, const std::size_t)>;
-		auto cmp_map = std::unordered_map<std::size_t, cmp>{
-			{items.cats, std::greater{}},
-			{items.trees, std::greater{}},
-			{items.pomeranians, std::less{}},
-			{items.goldfish, std::less{}}
-		};
-
-		auto real_sue = std::find_if(aunties.begin(), aunties.end(), [&sue, &differences, &cmp_map] (auto& aunt) {
-
-			auto other_sue = aunt.detectables;
-
-			auto items_of_interest = extract_items_of_interest(differences, other_sue);
-
-			auto is_subset = std::includes(sue.begin(), sue.end(), other_sue.begin(), other_sue.end());
-
-			auto is_found = std::accumulate(items_of_interest.begin(), items_of_interest.end(), is_subset,
-											[&differences, &cmp_map] (auto acc, auto& item_of_interest) {
-				auto item = differences.find(item_of_interest.first);
-				auto found_item = (item != differences.end());
-				return (acc && (found_item ? cmp_map[item->first](item_of_interest.second, item->second) : true));
-			});
-
-			return is_found;
-		});
-
-		std::cout << real_sue->id << std::endl;
+		std::cout << find_real_sue(aunties) << std::endl;
 
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
