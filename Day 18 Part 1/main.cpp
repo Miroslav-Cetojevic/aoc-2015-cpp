85f88bb2d85d9fb02311ae37ea8df51afb09534d
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-04 16:04:06 +0100

    Some refactoring...
    a) added boost::counting_range for the loops, b) some computations were extracted into lambdas, and c) a map to convert the characters from the input file to 0/1 for the array.

--- Day 18 Part 1/main.cpp
+++ Day 18 Part 1/main.cpp
@@ -4,59 +4,90 @@
 #include <numeric>
 #include <string>
 
-int main() {
-	std::ios_base::sync_with_stdio(false);
+#include <boost/range/counting_range.hpp>
 
-	auto filename = std::string{"grid.txt"};
+int main() {
+
+	const auto filename = std::string{"grid.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
 
-		constexpr auto gridlen = 102UL;
+		/*
+		 * this will create a "ring" buffer around the actual grid
+		 * allowing to bypass the need to treat first/last rows,
+		 * columns, and all four corners of the actual grid
+		 * in a special way
+		 */
+		constexpr auto gridlen = std::uint64_t{102};
 		constexpr auto gridsize = (gridlen * gridlen);
 
-		auto steps = 100UL;
-
-		auto grid = std::array<std::size_t, gridsize>{};
+		auto grid = std::array<std::uint64_t, gridsize>{};
 
 		std::string line;
-		for(auto row = 1UL; std::getline(file, line); ++row) {
 
-			for(auto col = 1UL; col <= line.size(); ++col) {
-				grid[row*gridlen+col] = line[col-1] - '0';
+		const auto index = [gridlen] (auto row, auto col) {
+			return (row * gridlen + col);
+		};
+
+		// to convert a character to a number in the array
+		auto char_map = std::unordered_map<char, std::uint64_t>{
+			{'.', 0},
+			{'#', 1}
+		};
+
+		for(auto row = std::uint64_t{1}; std::getline(file, line); ++row) {
+
+			for(auto col : boost::counting_range({1}, (line.size() + 1))) {
+
+				grid[index(row,col)] = char_map[line[col-1]];
+
 			}
 		}
 
+		const auto must_turn_off = [&grid] (auto pos, auto sum) {
+			return (grid[pos] == 1) && (sum < 2 || sum > 3);
+		};
+
+		const auto must_turn_on = [&grid] (auto pos, auto sum) {
+			return (grid[pos] == 0) && (sum == 3);
+		};
+
 		auto tmp_grid = grid;
 
-		auto limit = (gridlen - 1);
+		constexpr auto steps = std::uint64_t{100};
 
-		for(auto step = 0UL; step < steps; ++step) {
-			// traverse the grid
-			for(auto row = 1UL; row < limit; ++row) {
+		constexpr auto limit = (gridlen - 1);
 
-				for(auto col = 1UL; col < limit; ++col) {
+		for(const auto step : boost::counting_range({}, steps)) {
 
-					auto sum = grid[(row-1)*gridlen+(col-1)]
-								+ grid[(row-1)*gridlen+col]
-								+ grid[(row-1)*gridlen+(col+1)]
-								+ grid[row*gridlen+(col-1)]
-								+ grid[row*gridlen+(col+1)]
-								+ grid[(row+1)*gridlen+(col-1)]
-								+ grid[(row+1)*gridlen+col]
-								+ grid[(row+1)*gridlen+(col+1)];
+			for(const auto row: boost::counting_range({1}, limit)) {
 
-					auto pos = (row * gridlen + col);
+				for(const auto col : boost::counting_range({1}, limit)) {
 
-					if((grid[pos] == 1) && (sum < 2 || sum > 3)) { tmp_grid[pos] = 0; }
-					else if(grid[pos] == 0 && sum == 3) { tmp_grid[pos] = 1; }
+					const auto sum = grid[(row-1)*gridlen+(col-1)]
+									 + grid[(row-1)*gridlen+col]
+									 + grid[(row-1)*gridlen+(col+1)]
+									 + grid[row*gridlen+(col-1)]
+									 + grid[row*gridlen+(col+1)]
+									 + grid[(row+1)*gridlen+(col-1)]
+									 + grid[(row+1)*gridlen+col]
+									 + grid[(row+1)*gridlen+(col+1)];
+
+					const auto pos = index(row, col);
+
+					if(must_turn_off(pos, sum)) {
+						tmp_grid[pos] = 0;
+					} else if(must_turn_on(pos, sum)) {
+						tmp_grid[pos] = 1;
+					}
 				}
 			}
 
 			grid = tmp_grid;
 		}
 
-		auto count = std::accumulate(grid.begin(), grid.end(), 0U);
+		const auto count = std::accumulate(grid.begin(), grid.end(), std::uint64_t{});
 
 		std::cout << count << std::endl;
 
