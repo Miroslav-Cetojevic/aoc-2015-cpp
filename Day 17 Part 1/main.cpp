d096038ad3d69dd05157b09364a29c8b58387ba6
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-04 15:37:18 +0100

    Added a helper function...
    ... that returns a vector constructed with istream_iterators.

--- Day 17 Part 1/main.cpp
+++ Day 17 Part 1/main.cpp
@@ -3,11 +3,20 @@
  */
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <numeric>
 #include <string>
 #include <vector>
 
 #include <boost/range/counting_range.hpp>
+
+template<typename T>
+auto get_vector_from_file(std::fstream& file) {
+	return std::vector<T>{
+		std::istream_iterator<T>{file},
+		std::istream_iterator<T>{}
+	};
+}
 
 int main() {
 
@@ -16,19 +25,11 @@ int main() {
 
 	if(file.is_open()) {
 
-		auto containers = std::vector<std::uint64_t>{};
-
-		std::uint64_t container;
-
-		while(file >> container) {
-			containers.push_back(container);
-		}
+		const auto containers = get_vector_from_file<std::uint64_t>(file);
 
 		const auto target_volume = std::uint64_t{150};
 
 		auto count = std::uint64_t{};
-
-		const auto size = containers.size();
 
 		/*
 		 * NOTE: the following algorithm was adapted
@@ -37,13 +38,15 @@ int main() {
 		 */
 
 		/* compute power set */
+		const auto size = containers.size();
+
 		auto powerset = std::vector<std::vector<std::uint64_t>>(1 << size);
 
 		powerset[0] = {};
 
 		for(const auto i : boost::counting_range({}, size)) {
 
-			const auto subsize = std::uint64_t{1} << i; // doubling size of subset
+			const auto subsize = (1 << i); // doubling size of subset
 
 			for(const auto j : boost::counting_range({}, subsize)) {
 
