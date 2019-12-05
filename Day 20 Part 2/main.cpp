94ad5906a5f67fec9890a69d2b5f96b8015590eb
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-05 23:22:59 +0100

    Added boost::counting_range()

--- Day 20 Part 2/main.cpp
+++ Day 20 Part 2/main.cpp
@@ -1,38 +1,43 @@
+/*
+ * NOTE: this solution was taken straight from
+ * https://www.reddit.com/r/adventofcode/comments/3xjpp2/day_20_solutions/
+ */
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-//https://www.reddit.com/r/adventofcode/comments/3xjpp2/day_20_solutions/
+#include <boost/range/counting_range.hpp>
+
 int main() {
 
-	auto total_presents = 33100000UL;
-	auto n_presents = 11UL;
-	auto n_houses = (total_presents / n_presents);
-	auto max_houses = 50UL;
+	const auto total_presents = 33100000;
+	const auto n_presents = 11;
+	const auto total_houses = (total_presents / n_presents);
+	const auto max_houses = 50;
 
 	// array with 3 mil elements will smash the stack, so vector is used instead
-	auto container_size = (n_houses + 1);
-	auto houses = std::vector<std::size_t>(container_size, n_presents);
+	// every house has at least 11 presents because the first elf visits them all
+	auto houses = std::vector(total_houses, n_presents);
 
-	// all the houses are initialized with 10 presents, so we don't need the first elf
-	// elf_id is one-based, house_id && houses_visited are zero_based
-	for(auto elf_id = 2UL; elf_id <= n_houses; ++elf_id) {
+	// elf_id is one-based, but house_id is zero-based because it's used to access the vector
+	for(const auto elf_id : boost::counting_range(2, (total_houses + 1))) {
 
-		for(auto house_id = (elf_id - 1), houses_visited = 0UL;
-			(houses_visited < max_houses) && (house_id < n_houses);
+		for(auto house_id = (elf_id - 1), houses_visited = 0;
+			(houses_visited < max_houses) && (house_id < total_houses);
 			house_id += elf_id, ++houses_visited) {
 
 			houses[house_id] += (elf_id * n_presents);
 		}
 	}
 
-	auto lowest_house = std::next(std::find_if(houses.begin(), houses.end(), [total_presents] (auto house) {
+	const auto lowest_house = std::find_if(houses.begin(), houses.end(), [total_presents] (auto house) {
 		return (house >= total_presents);
-	}));
+	});
 
-	auto min_house_number = std::distance(houses.begin(), lowest_house);
+	// add 1 to account for zero-based indexing of vector
+	const auto house_number = std::distance(houses.begin(), lowest_house) + 1;
 
-	std::cout << min_house_number << std::endl;
+	std::cout << house_number << std::endl;
 
 	return 0;
 }
