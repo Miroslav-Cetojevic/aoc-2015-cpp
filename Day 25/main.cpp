1abf74df4a3b8c9ba09cf315cd411728c376b11d
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-11 02:17:42 +0100

    Added comments to explain the code ...
    ... plus some refactoring.

--- Day 25/main.cpp
+++ Day 25/main.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 #include <numeric>
 
+#include <boost/range/irange.hpp>
+
+template<typename T = std::uint64_t>
 constexpr auto get_code() {
-	auto row = 3010UL;
-	auto col = 3019UL;
 
-	auto init_value = 20151125UL;
-	auto multiplier = 252533UL;
-	auto modulus = 33554393UL;
+	const T row = 3010;
+	const T col = 3019;
 
-	auto n = (row + col - 1);
-	auto sum_formula = ((n * (n + 1)) / 2);
+	const T init_value = 20151125;
+	const T multiplier = 252533;
+	const T modulus    = 33554393;
 
-	// target is a triangular number:  (x,y) => (1,x+y-1) - x + 1
-	auto target = (sum_formula - row + 1);
+	// the number at (row,col) represents the position
+	// in the sequence of natural numbers that form
+	// a triangle, as seen in the readme.txt
+	const auto n = (row + col - 1);
+
+	// this is the final number in the triangular arrangement
+	// that the number at (row,col) is part of
+	const auto triangular_number = ((n * (n + 1)) / 2);
+
+	// target represents the end of the sequence we need to
+	// go through in order to compute the value at (row,col).
+	// this corresponds to the following mapping (see example
+	// table in the readme.txt):
+	// (x,y) => triangular(1,(x+y-1)) - x + 1
+	const auto target = (triangular_number - row + 1);
+
 	auto result = init_value;
 
-	auto i = 1UL;
-	while((i++) < target) {
+	for(const auto i : boost::irange({1}, target)) {
 		result *= multiplier;
 		result %= modulus;
 	}
