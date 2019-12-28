868fb0d72a2afabc4f4e4330659316fde2bbc3a7
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-28 23:54:45 +0100

    Replaced raw loop with std::accumulate

--- Day 01 Part 1/main.cpp
+++ Day 01 Part 1/main.cpp
@@ -1,28 +1,28 @@
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include <string>
 
 int main() {
 
-	auto filename = std::string{"directions.txt"};
+	const auto filename = std::string{"directions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
 
-		auto floor = 0;
-		auto UP = '(';
-		decltype(UP) direction;
+		using iterator = std::istreambuf_iterator<char>;
 
-		while(file >> direction) {
+		const auto begin = iterator{file};
+		const auto end   = iterator{};
 
-			if(direction == UP) {
-				++floor;
-			} else {
-				--floor;
-			}
-		}
+		const auto accumulator = [UP = '('] (auto floor, const auto direction) {
+			return floor += (direction == UP) ? 1 : -1;
+		};
 
-		std::cout << floor << std::endl;
+		const auto result = std::accumulate(begin, end, 0, accumulator);
+
+		std::cout << result << std::endl;
 
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
