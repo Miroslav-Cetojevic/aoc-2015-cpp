cb709fab688c7016aaa454bf77ec2c6daf71ed70
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-29 12:39:49 +0100

    Added std::sort

--- Day 02 Part 1/main.cpp
+++ Day 02 Part 1/main.cpp
@@ -17,12 +17,36 @@ auto& operator>>(std::istream& in, Dimensions& D) {
 	return in >> D.length >> c >> D.width >> c >> D.height;
 }
 
-auto get_area_list(const Dimensions& D) {
-	return std::vector{
-		(D.length * D.width),
-		(D.width * D.height),
-		(D.length * D.height)
+auto get_sorted_area_list(const Dimensions& D) {
+	const auto length = D.length;
+	const auto width  = D.width;
+	const auto height = D.height;
+	auto list = std::vector{
+		(length * width),
+		(width * height),
+		(length * height)
 	};
+	sort(list.begin(), list.end());
+	return list;
+}
+
+auto get_total_paper(std::fstream& file) {
+	using iterator = std::istream_iterator<Dimensions>;
+
+	const auto begin = iterator{file};
+	const auto end   = iterator{};
+	const auto init  = uint64{};
+
+	const auto accumulator = [] (auto acc, const auto& D) {
+		const auto list  = get_sorted_area_list(D);
+		const auto begin = list.begin();
+		const auto end   = list.end();
+		const auto init  = uint64{};
+		// list is sorted, so the first element will always be the smallest
+		return acc += (2 * std::accumulate(begin, end, init) + list.front());
+	};
+
+	return std::accumulate(begin, end, init, accumulator);
 }
 
 int main() {
@@ -32,23 +56,7 @@ int main() {
 
 	if(file.is_open()) {
 
-		using iterator = std::istream_iterator<Dimensions>;
-
-		const auto begin = iterator{file};
-		const auto end   = iterator{};
-		const auto init  = uint64{};
-
-		const auto accumulator = [] (auto acc, const auto& D) {
-			const auto list  = get_area_list(D);
-			const auto begin = list.begin();
-			const auto end   = list.end();
-			const auto init  = uint64{};
-			return acc += (2 * std::accumulate(begin, end, init) + *std::min_element(begin, end));
-		};
-
-		const auto total_paper = std::accumulate(begin, end, init, accumulator);
-
-		std::cout << total_paper << std::endl;
+		std::cout << get_total_paper(file) << std::endl;
 
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
