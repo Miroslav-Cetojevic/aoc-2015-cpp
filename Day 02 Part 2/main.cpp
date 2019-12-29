8bc10510ce09fae4e9a9c71dc8fcb7d66e9f1683
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-29 12:41:37 +0100

    Changes are similar to Part 1

--- Day 02 Part 2/main.cpp
+++ Day 02 Part 2/main.cpp
@@ -1,44 +1,64 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include <string>
-#include <utility>
+#include <vector>
+
+using uint64 = std::uint64_t;
 
 struct Dimensions {
-	unsigned length, width, height;
+	uint64 length, width, height;
 };
 
 auto& operator>>(std::istream& in, Dimensions& D) {
-	return in >> D.length >> D.width >> D.height;
+	char c; // throwaway input
+	return in >> D.length >> c >> D.width >> c >> D.height;
+}
+
+auto get_sorted_sides_list(const Dimensions& D) {
+	auto list = std::vector{D.length, D.width, D.height};
+	std::sort(list.begin(), list.end());
+	return list;
+}
+
+auto get_total_ribbon(std::fstream& file) {
+	using iterator = std::istream_iterator<Dimensions>;
+
+	const auto begin = iterator{file};
+	const auto end   = iterator{};
+	const auto init  = uint64{};
+
+	const auto accumulator = [] (auto acc, const auto& D) {
+
+		const auto list  = get_sorted_sides_list(D);
+
+		const auto begin = list.begin();
+		const auto end   = list.end();
+
+		const auto init_plus = uint64{};
+		const auto init_multiply = uint64{1};
+
+		// the list is sorted, i.e. the longest side is always the last element, so
+		// we can subtract that from the sum of all sides to get the two smallest sides
+		const auto ribbon_present = 2 * (std::accumulate(begin, end, init_plus) - list.back());
+		const auto ribbon_bow = std::accumulate(begin, end, init_multiply, std::multiplies{});
+
+		return acc += (ribbon_present + ribbon_bow);
+	};
+
+	return std::accumulate(begin, end, init, accumulator);
 }
 
 int main() {
 
-	auto filename = std::string{"dimensions.txt"};
+	const auto filename = std::string{"dimensions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
 
-		auto total_ribbon = 0U;
-
-		Dimensions D;
-
-		while(file >> D) {
-
-			auto& l = D.length;
-			auto& w = D.width;
-			auto& h = D.height;
-
-			// get the two smallest sides, min1 & min2
-			if(l > w) { std::swap(l, w); }
-
-			auto min1 = l;
-			auto min2 = std::min(w, h);
-
-			total_ribbon += 2 * (min1 + min2) + (l * w * h);
-		}
-
-		std::cout << total_ribbon << std::endl;
+		std::cout << get_total_ribbon(file) << std::endl;
 
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
