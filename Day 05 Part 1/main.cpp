b25caefaae42bf5c18778f784a9b028629eef32a
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-30 16:07:48 +0100

    Refactoring: see description for more details
    - replaced std::getline with std::istream_iterator to use with std::accumulate
    - replaced function parameters of type std:string with std::string_view
    - added boost::irange
    - removed std::vector

--- Day 05 Part 1/main.cpp
+++ Day 05 Part 1/main.cpp
@@ -1,68 +1,75 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include <string>
-#include <vector>
+#include <string_view>
 
-auto has_bad_substring(const std::string& string) {
+auto has_bad_substring(const std::string_view& str) {
 
-	static const auto bad_strings = std::vector<std::string>{ {"ab"},
-															  {"cd"},
-															  {"pq"},
-															  {"xy"} };
+	// bad strings are "ab", "cd", "pq" and "xy", and we can see that the
+	// 2nd letter in each of them is always ahead by one in the alphabet
+	const auto predicate = [bad = std::string_view{"acpx"}] (auto a, auto b) {
+		return (b == (a + 1)) && (bad.find(a) != bad.npos);
+	};
 
-	auto is_bad = std::any_of(bad_strings.begin(), bad_strings.end(), [&string] (const auto& bad_string) {
-		return (string.find(bad_string) != string.npos);
+	const auto result = std::adjacent_find(str.begin(), str.end(), predicate);
+
+	return (result != str.end());
+}
+
+auto has_adjacent_twin_letters(const std::string_view& str) {
+
+	const auto result = std::adjacent_find(str.begin(), str.end());
+
+	return (result != str.end());
+}
+
+auto has_triple_vowels(const std::string_view& str) {
+
+	const auto vowels = std::string_view{"aeiou"};
+
+	const auto predicate = [&vowels, count = 0] (const auto c) mutable {
+		count += (vowels.find(c) != vowels.npos);
+
+		return (count >= 3);
+	};
+
+	const auto result = std::find_if(str.begin(), str.end(), predicate);
+
+	return (result != str.end());
+}
+
+auto is_nice_string(const std::string_view& str) {
+
+	return !has_bad_substring(str)
+		   && has_adjacent_twin_letters(str)
+		   && has_triple_vowels(str);
+}
+
+auto count_nice_strings(std::fstream& file) {
+
+	using iterator = std::istream_iterator<std::string>;
+
+	const auto begin = iterator{file};
+	const auto end   = iterator{};
+
+	const auto result = std::accumulate(begin, end, 0, [] (auto acc, const auto& line) {
+		return (acc + is_nice_string(line));
 	});
 
-	return is_bad;
-}
-
-auto has_adjacent_twin_letters(const std::string& string) {
-
-	auto result = std::adjacent_find(string.begin(), string.end());
-
-	return (result != string.end());
-}
-
-auto has_triple_vowels(const std::string& string) {
-
-	auto result = std::count_if(string.begin(), string.end(), [] (const auto& c) {
-
-		auto has_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-
-		return has_vowel;
-	});
-
-	return (result >= 3);
-}
-
-auto is_nice_string(const std::string& string) {
-
-	return !has_bad_substring(string)
-		   && has_adjacent_twin_letters(string)
-		   && has_triple_vowels(string);
+	return result;
 }
 
 int main() {
 
-	auto filename = std::string{"strings.txt"};
+	const auto filename = std::string{"strings.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
 
-		auto count = 0U;
-
-		std::string line;
-
-		while(std::getline(file, line)) {
-
-			if(is_nice_string(line)) {
-				++count;
-			}
-		}
-
-		std::cout << count << std::endl;
+		std::cout << count_nice_strings(file) << std::endl;
 
 	} else {
 		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
