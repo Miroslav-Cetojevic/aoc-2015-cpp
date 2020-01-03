85b1db37b00bdc9b3f1dacb6ec24af4a1822c8c9
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2020-01-03 15:39:58 +0100

    Some refactoring and additional comments

--- Day 08 Part 2/main.cpp
+++ Day 08 Part 2/main.cpp
@@ -1,48 +1,63 @@
-#include <fstream>
 #include <iostream>
-#include <iterator>
-#include <string>
+
+#include "puzzle_input.hpp"
+
+auto solution = [] (auto input) {
+
+	auto encsum = std::size_t{};
+	auto nlsum = encsum;
+
+	const auto backslash = '\\';
+
+	for(auto it = input.begin(); it != input.end(); ++it) {
+		auto c = *it;
+		if(c == backslash) {
+			c = *(++it);
+			if(c == backslash || c == '\"') {
+				encsum += 2;
+			} else {
+				++encsum;
+				it += 2;
+			}
+		} else if(c == '\n') {
+			++nlsum;
+		}
+	}
+
+	/* the return expression is shortened from the following:
+	 *
+	 * const auto codesum = (input.size() - nlsum);
+	 * const auto encodesum = codesum + (4 * nlsum) + encsum + 4;
+	 * const auto result = (encodesum - codesum);
+	 *
+	 * a) the codesum is the sum of all characters minus the newlines, since these are
+	 * 	  not counted in the code.
+	 * b) the encoded list contains besides the codesum the sum of additional characters
+	 *    for the encoding (encsum) as well as the newlines and the double quotes at the
+	 *    beginning and end of the list (also encoded)
+	 *    b1) with the encoding, newline goes from ["\n", 1 char] to ["\\\n", 4 chars]
+	 *    b2) there are two double quotes not encoded in the loop, they add up to another
+	 *        4 chars
+	 *
+	 * Taking all of the above into account, the result can be expanded to:
+	 *
+	 * 1. (input.size() - nlsum) + (4 * nlsum) + encsum + 4 - (input.size() - nlsum)
+	 * 2. input.size() + (4 * nlsum - nlsum) + encsum + 4 - input.size() + nlsum
+	 *
+	 * Rearrange the operands in an equivalent fashion:
+	 *
+	 * 3. (input.size() - input.size()) + (4 * nlsum - nlsum + nlsum) + encsum + 4
+	 *
+	 * Final result:
+	 *
+	 * 4. (4 * nlsum) + encsum + 4
+	 *
+	 */
+	return (4 * nlsum) + encsum + 4;
+};
 
 int main() {
 
-	const auto filename = std::string{"list.txt"};
-	auto file = std::fstream{filename};
+	std::cout << solution(puzzle_input) << std::endl;
 
-	if(file.is_open()) {
-
-		const auto list = std::string{
-			std::istreambuf_iterator<char>{file},
-			std::istreambuf_iterator<char>{}
-		};
-
-		auto encsum = unsigned{};
-		auto nlsum = encsum;
-
-		for(auto it = list.begin(); it != list.end(); ++it) {
-			auto c = *it;
-			if(c == '\n') { ++nlsum; }
-			if(c == '\\') {
-				c = *(++it);
-				if(c == '\\' || c == '\"') {
-					// (new)codesum will already contain the sum of original
-					// escape sequence lengths, so we just add the new encodings
-					encsum += 2;
-				} else {
-					++encsum;
-					it += 2;
-				}
-			}
-		}
-
-		auto codesum = list.size() - nlsum;
-		auto new_codesum = codesum + (4 * nlsum) + encsum;
-		auto result = (new_codesum - codesum);
-
-		std::cout << result << std::endl;
-
-	} else {
-		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
-	}
-
-	return 0;
 }
