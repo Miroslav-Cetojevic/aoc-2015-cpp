8bc8535e2be6bfdbf2dd9939be86c933081eb63d
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2020-01-10 17:58:55 +0100

    A more efficient approach

--- Day 11 Part 1/main.cpp
+++ Day 11 Part 1/main.cpp
@@ -1,96 +1,63 @@
 #include <algorithm>
-#include <functional>
 #include <iostream>
 #include <string>
 #include <string_view>
 
-#include <boost/range/adaptor/reversed.hpp>
-#include <boost/range/counting_range.hpp>
+bool has_two_pairs(std::string_view pwd) {
+  const auto begin = pwd.begin();
+  const auto end   = pwd.end();
 
-constexpr auto first_letter = 'a';
-constexpr auto last_letter = 'z';
+  const auto first_pair = std::adjacent_find(begin, end);
 
-template<typename P>
-constexpr auto increment_pwd(P& pwd) {
-
-	for(auto& c : boost::reversed_range{pwd}) {
-
-		if(c != last_letter) {
-			++c;
-			break;
-		}
-
-		c = first_letter;
-	}
+  return (first_pair < (end - 3)) and (std::adjacent_find((first_pair + 2), end) != end);
 }
 
-template<typename P>
-constexpr auto is_incrementable(const P& pwd) {
-
-	const auto count = std::count_if(pwd.begin(), pwd.end(), [] (const auto letter) {
-		return (letter == last_letter);
-	});
-
-	const auto size = pwd.size();
-
-	return (static_cast<decltype(size)>(count) != size);
+bool has_incrementing_triple(std::string_view pwd) {
+  const auto size = pwd.size();
+  auto is_triple = false;
+  for(auto i = decltype(size){2}; not is_triple and (i < size); ++i) {
+    is_triple = (pwd[i] == (pwd[i-1] + 1)) and (pwd[i-1] == (pwd[i-2] + 1));
+  }
+  return is_triple;
 }
 
-template<typename P>
-constexpr auto has_two_pairs(const P& pwd) {
-
-	const auto first_result = std::adjacent_find(pwd.begin(), pwd.end());
-	const auto has_one_pair = (first_result != pwd.end());
-
-	const auto second_result = std::adjacent_find((first_result + 2), pwd.end());
-	const auto has_another_pair = (second_result != pwd.end());
-
-	return has_one_pair && has_another_pair;
+auto is_not_banned (std::string_view pwd) {
+  const auto result = std::find_if(pwd.begin(), pwd.end(), [] (const auto c) {
+    return (c == 'i' || c == 'o' || c == 'l');
+  });
+  return (result == pwd.end());
 }
 
-template<typename P>
-constexpr auto has_three_straight(const P& pwd) {
-
-	auto pwdlen = pwd.length();
-
-	const auto range = boost::counting_range<decltype(pwdlen)>(2, pwdlen);
-
-	return std::any_of(range.begin(), range.end(), [&pwd] (auto i) {
-		return ((pwd[i]-1) == pwd[i-1]) && ((pwd[i]-2) == pwd[i-2]);
-	});
+auto valid(std::string_view pwd) {
+  return has_two_pairs(pwd)
+         and has_incrementing_triple(pwd)
+         and is_not_banned(pwd);
 }
 
-template<typename P>
-constexpr auto has_banned_letter(const P& pwd) {
-
-	const auto banned_letter = std::find_if(pwd.begin(), pwd.end(), [] (auto c) {
-		return c == 'i' || c == 'o' || c == 'l';
-	});
-
-	return (banned_letter != pwd.end());
+auto increment(std::string& pwd) {
+  auto index = (pwd.size() - 1);
+  while(pwd[index] == 'z') {
+    pwd[index--] = 'a';
+  }
+  ++pwd[index];
 }
 
-template<typename P>
-constexpr auto next_pwd(P& pwd) {
+auto next_pwd(std::string& pwd) {
+  while(not valid(pwd)) {
+    increment(pwd);
+  }
+}
 
-	auto not_secure = true;
-
-	while(not_secure && is_incrementable(pwd)) {
-
-		increment_pwd(pwd);
-
-		not_secure = (has_banned_letter(pwd) || !(has_two_pairs(pwd) && has_three_straight(pwd)));
-
-	}
+auto solution(std::string pwd) {
+  next_pwd(pwd);
+  return pwd;
 }
 
 int main() {
 
-	auto pwd = std::string{"cqjxjnds"};
+	const auto pwd = std::string{"cqjxjnds"};
 
-	next_pwd(pwd);
-
-	std::cout << pwd << std::endl;
+	std::cout << solution(pwd) << std::endl;
 
 	return 0;
 }
