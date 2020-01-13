7bfb560d1f77647ef9aafaf81ff399d8350bf84d
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2020-01-13 12:15:32 +0100

    Forgot to upload this one before Part 2

--- Day 14 Part 1/main.cpp
+++ Day 14 Part 1/main.cpp
@@ -1,67 +1,62 @@
 #include <algorithm>
-#include <fstream>
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <sstream>
 #include <string>
 
+#include "input.hpp"
+
+using uintmax = std::uintmax_t;
+
 struct Reindeer {
-	std::string name;
-	unsigned speed;
-	unsigned runtime;
-	unsigned resttime;
+  uintmax speed,
+          runtime,
+          resttime;
 };
 
 auto& operator>>(std::istream& in, Reindeer& r) {
-	return in >> r.name >> r.speed >> r.runtime >> r.resttime;
+  std::string s; // throwaway input
+  return in >> s >> s >> s
+            >> r.speed >> s >> s
+            >> r.runtime >> s >> s >> s >> s >> s >> s
+            >> r.resttime >> s;
+}
+
+auto solution(const std::string& input) {
+
+  auto stream = std::stringstream{input};
+
+  using iterator = std::istream_iterator<Reindeer>;
+
+  auto begin = iterator{stream};
+  auto end   = iterator{};
+  auto init  = uintmax{};
+
+  const auto max = [seconds = uintmax{2503}] (const auto current_max, const auto& reindeer) {
+
+    const auto runtime = reindeer.runtime;
+    const auto speed = reindeer.speed;
+
+    const auto distance = (speed * runtime);
+    const auto cycle = (runtime + reindeer.resttime);
+    const auto cycles = (seconds / cycle);
+    const auto time_left = (seconds % cycle);
+
+    // has this reindeer enough time for another full sprint before resting?
+    const auto can_do_full_sprint = (time_left >= runtime);
+
+    const auto tmp_distance = (distance * cycles)
+                              + (can_do_full_sprint ? distance : (speed * time_left));
+
+    return std::max(current_max, tmp_distance);
+  };
+
+  return std::accumulate(begin, end, init, max);
 }
 
 int main() {
 
-	const auto filename = std::string{"reindeers.txt"};
-	auto file = std::fstream{filename};
+	std::cout << solution(puzzle_input) << std::endl;
 
-	if(file.is_open()) {
-
-		unsigned seconds;
-
-		file >> seconds;
-
-		auto begin = std::istream_iterator<Reindeer>{file};
-		auto end   = std::istream_iterator<Reindeer>{};
-		auto init  = unsigned{};
-		auto max   = [seconds] (const auto current_max, const auto& reindeer) {
-
-			const auto runtime = reindeer.runtime;
-			const auto speed = reindeer.speed;
-
-			const auto distance = (speed * runtime);
-			const auto cycle = (runtime + reindeer.resttime);
-			const auto cycles = (seconds / cycle);
-			const auto time_left = (seconds % cycle);
-
-			auto tmp_distance = (distance * cycles);
-
-			// has this reindeer enough time for another full sprint before resting?
-			const auto can_do_full_sprint = (time_left >= runtime);
-
-			if(can_do_full_sprint) {
-				tmp_distance += distance;
-			} else {
-				tmp_distance += (speed * time_left);
-			}
-
-			return std::max(current_max, tmp_distance);
-		};
-
-		// std::accumulate can be used to compute the minmax or a sequence
-		const auto max_distance = std::accumulate(begin, end, init, max);
-
-		std::cout << max_distance << std::endl;
-
-	} else {
-		std::cerr << "Error! Could not open " << filename << "!" << std::endl;
-	}
-
-	return 0;
 }
