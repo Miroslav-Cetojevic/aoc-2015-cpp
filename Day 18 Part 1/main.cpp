dc147215d17036c5480586d1efa4d2e92915b607
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2020-01-16 00:20:19 +0100

    A bit of refactoring and change of types
    - string_view instead of string
    - char instead of uint64_t

--- Day 18 Part 1/main.cpp
+++ Day 18 Part 1/main.cpp
@@ -1,99 +1,124 @@
 #include <array>
-#include <fstream>
 #include <iostream>
 #include <numeric>
-#include <string>
+#include <string_view>
 
-#include <boost/range/counting_range.hpp>
+#include <boost/range/irange.hpp>
+
+#include "input.hpp"
+
+// compile-time parsing of string_view
+struct ParseStats {
+  int num_cols,
+      num_rows;
+};
+
+constexpr auto NEWLINE = '\n';
+
+constexpr auto parse_stats(std::string_view input) {
+  auto stats = ParseStats{};
+
+  auto& num_cols = stats.num_cols;
+  auto& num_rows = stats.num_rows;
+
+  for(const auto c : input) {
+    num_cols += (c != NEWLINE);
+    num_rows += (c == NEWLINE);
+  }
+
+  ++num_rows;
+  num_cols /= num_rows;
+
+  return stats;
+}
+
+constexpr auto PARSE_STATS = parse_stats(puzzle_input);
+
+constexpr auto NUM_COLS = PARSE_STATS.num_cols;
+constexpr auto NUM_ROWS = PARSE_STATS.num_rows;
+
+constexpr auto GRIDLEN = NUM_COLS + 2;
+constexpr auto GRIDSIZE = (GRIDLEN * GRIDLEN);
+
+using Grid = std::array<char, GRIDSIZE>;
+
+const auto my_index = [] (auto row, auto col) {
+  return (row * GRIDLEN + col);
+};
+
+auto parse(std::string_view input) {
+
+  auto grid = Grid{};
+
+  for(const auto row : boost::irange(1, NUM_ROWS+1)) {
+
+    for(const auto col : boost::irange(1, NUM_COLS+1)) {
+
+      if(input.front() == '#') {
+        grid[my_index(row, col)] = 1;
+      }
+
+      input.remove_prefix(1);
+    }
+    // skip newline
+    input.remove_prefix(1);
+  }
+
+  return grid;
+}
+
+auto num_lights_on(Grid grid, const unsigned steps) {
+
+  auto tmp_grid = grid;
+
+  constexpr auto limit = (GRIDLEN - 1);
+
+  for(const auto step : boost::irange(steps)) {
+
+    for(const auto row: boost::irange(1, limit)) {
+
+      for(const auto col : boost::irange(1, limit)) {
+
+        const auto sum = grid[my_index(row-1,col-1)]
+                       + grid[my_index(row-1,col)]
+                       + grid[my_index(row-1,col+1)]
+                       + grid[my_index(row,col-1)]
+                       + grid[my_index(row,col+1)]
+                       + grid[my_index(row+1,col-1)]
+                       + grid[my_index(row+1,col)]
+                       + grid[my_index(row+1,col+1)];
+
+        const auto pos = my_index(row, col);
+
+        const auto must_turn_off = [&] () {
+          return (grid[pos]) && (sum < 2 || sum > 3);
+        };
+
+        const auto must_turn_on = [&] () {
+          return (not grid[pos]) && (sum == 3);
+        };
+
+        if(must_turn_off()) {
+          tmp_grid[pos] = 0;
+        } else if(must_turn_on()) {
+          tmp_grid[pos] = 1;
+        }
+      }
+    }
+
+    grid = tmp_grid;
+  }
+
+  return std::accumulate(grid.begin(), grid.end(), 0);
+}
+
+auto solution(std::string_view input, const unsigned steps) {
+
+  const auto& grid = parse(input);
+
+  return num_lights_on(grid, steps);
+}
 
 int main() {
-
-	const auto filename = std::string{"grid.txt"};
-	auto file = std::fstream{filename};
-
-	if(file.is_open()) {
-
-		/*
-		 * this will create a "ring" buffer around the actual grid
-		 * allowing to bypass the need to treat first/last rows,
-		 * columns, and all four corners of the actual grid
-		 * in a special way
-		 */
-		constexpr auto gridlen = std::uint64_t{102};
-		constexpr auto gridsize = (gridlen * gridlen);
-
-		auto grid = std::array<std::uint64_t, gridsize>{};
-
-		const auto index = [gridlen] (auto row, auto col) {
-			return (row * gridlen + col);
-		};
-
-		// to convert a character to a number in the array
-		auto char_map = std::unordered_map<char, std::uint64_t>{
-			{'.', 0},
-			{'#', 1}
-		};
-
-		std::string line;
-
-		for(auto row = std::uint64_t{1}; std::getline(file, line); ++row) {
-
-			for(auto col : boost::counting_range({1}, (line.size() + 1))) {
-
-				grid[index(row,col)] = char_map[line[col-1]];
-
-			}
-		}
-
-		const auto must_turn_off = [&grid] (auto pos, auto sum) {
-			return (grid[pos] == 1) && (sum < 2 || sum > 3);
-		};
-
-		const auto must_turn_on = [&grid] (auto pos, auto sum) {
-			return (grid[pos] == 0) && (sum == 3);
-		};
-
-		auto tmp_grid = grid;
-
-		constexpr auto steps = std::uint64_t{100};
-
-		constexpr auto limit = (gridlen - 1);
-
-		for(const auto step : boost::counting_range({}, steps)) {
-
-			for(const auto row: boost::counting_range({1}, limit)) {
-
-				for(const auto col : boost::counting_range({1}, limit)) {
-
-					const auto sum = grid[(row-1)*gridlen+(col-1)]
-									 + grid[(row-1)*gridlen+col]
-									 + grid[(row-1)*gridlen+(col+1)]
-									 + grid[row*gridlen+(col-1)]
-									 + grid[row*gridlen+(col+1)]
-									 + grid[(row+1)*gridlen+(col-1)]
-									 + grid[(row+1)*gridlen+col]
-									 + grid[(row+1)*gridlen+(col+1)];
-
-					const auto pos = index(row, col);
-
-					if(must_turn_off(pos, sum)) {
-						tmp_grid[pos] = 0;
-					} else if(must_turn_on(pos, sum)) {
-						tmp_grid[pos] = 1;
-					}
-				}
-			}
-
-			grid = tmp_grid;
-		}
-
-		const auto count = std::accumulate(grid.begin(), grid.end(), std::uint64_t{});
-
-		std::cout << count << std::endl;
-
-	} else {
-		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
-	}
-
-	return 0;
+  std::cout << solution(puzzle_input, 100) << std::endl;
 }
