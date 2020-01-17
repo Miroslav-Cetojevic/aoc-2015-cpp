b4d7f80b6caa406d0b8dd82e820a1a7a1be24916
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2020-01-17 15:09:47 +0100

    Changes are the same as Part 1
    This time, instead of creating three loops to avoid the corners, I just turn them on before the first step and at the end of each subsequent step.

--- Day 18 Part 2/main.cpp
+++ Day 18 Part 2/main.cpp
@@ -1,123 +1,145 @@
 #include <array>
-#include <fstream>
 #include <iostream>
 #include <numeric>
-#include <string>
+#include <string_view>
 
-#include <boost/range/counting_range.hpp>
+#include <boost/range/irange.hpp>
+
+#include "input.hpp"
+
+// compile-time parsing of string_view
+struct ParseStats {
+  int num_cols,
+      num_rows;
+};
+
+constexpr auto NEWLINE = '\n';
+
+constexpr auto parse_stats(std::string_view input) {
+  auto stats = ParseStats{};
+
+  auto& num_cols = stats.num_cols;
+  auto& num_rows = stats.num_rows;
+
+  for(const auto c : input) {
+    num_cols += (c != NEWLINE);
+    num_rows += (c == NEWLINE);
+  }
+
+  ++num_rows;
+  num_cols /= num_rows;
+
+  return stats;
+}
+
+constexpr auto DEFAULT_INPUT = puzzle_input;
+constexpr auto DEFAULT_STEPS = 100;
+constexpr auto PARSE_STATS = parse_stats(DEFAULT_INPUT);
+
+/*
+ * This will create a "ring" buffer around the actual grid, allowing us to
+ * bypass the need to treat first/last rows/columns and all four corners of the
+ * actual grid as special cases.
+ */
+constexpr auto GRIDLEN = PARSE_STATS.num_cols + 2;
+constexpr auto GRIDWIDTH = PARSE_STATS.num_rows + 2;
+
+constexpr auto GRIDSIZE = (GRIDLEN * GRIDWIDTH);
+
+using Grid = std::array<char, GRIDSIZE>;
+
+const auto my_index = [] (auto row, auto col) {
+  return (row * GRIDLEN + col);
+};
+
+auto parse(std::string_view input) {
+
+  auto grid = Grid{};
+
+  for(const auto row : boost::irange(1, GRIDWIDTH-1)) {
+
+    for(const auto col : boost::irange(1, GRIDLEN-1)) {
+
+      if(input.front() == '#') {
+        grid[my_index(row, col)] = 1;
+      }
+
+      input.remove_prefix(1);
+    }
+    // skip newline
+    input.remove_prefix(1);
+  }
+
+  return grid;
+}
+
+auto num_lights_on(Grid grid, const unsigned steps) {
+
+  /*
+   * Each cell in the sums grid represents the sum of turned-on lights from
+   * grid[i-1] to grid[i+1]. The sum of three of these cells minus the current
+   * cell represents the sum of all turned-on lights in the neighborhood.
+   *
+   * Because we are only looking at the current cell in any iteration, we can
+   * overwrite the grid in-place.
+   */
+  auto sums = Grid{};
+
+  const auto turn_on_corners = [&grid, OTHERLEN = (2*GRIDLEN)] {
+    // top-left corner
+    grid[GRIDLEN+1] =
+    // top-right corner
+    grid[OTHERLEN-2] =
+    // bottom-left corner
+    grid[GRIDSIZE-OTHERLEN+1] =
+    // bottom-right corner
+    grid[GRIDSIZE-GRIDLEN-2] = 1;
+  };
+
+  turn_on_corners();
+
+  for(const auto step : boost::irange(steps)) {
+
+    for(const auto i : boost::irange(GRIDLEN, GRIDSIZE-GRIDLEN)) {
+      sums[i] = grid[i-1]
+              + grid[i]
+              + grid[i+1];
+    }
+
+    for(const auto row: boost::irange(1, (GRIDWIDTH - 1))) {
+
+      for(const auto col : boost::irange(1, (GRIDLEN - 1))) {
+
+        const auto pos = my_index(row, col);
+
+        auto& cell = grid[pos];
+
+        const auto sum = sums[pos]
+                       - cell
+                       + sums[pos+GRIDLEN]
+                       + sums[pos-GRIDLEN];
+
+        cell = (sum == 3) or (sum == 2 and cell == 1);
+
+      }
+    }
+    turn_on_corners();
+  }
+
+  return std::accumulate(grid.begin(), grid.end(), 0);
+}
+
+auto solution(std::string_view input, const unsigned steps) {
+
+  const auto& grid = parse(input);
+
+  const auto result = num_lights_on(grid, steps);
+
+  return result;
+}
 
 int main() {
 
-	const auto filename = std::string{"grid.txt"};
-	auto file = std::fstream{filename};
+  std::cout << solution(DEFAULT_INPUT, DEFAULT_STEPS) << std::endl;
 
-	if(file.is_open()) {
-
-		/*
-		 * this will create a "ring" buffer around the actual grid
-		 * allowing to bypass the need to treat first/last rows,
-		 * columns, and all four corners of the actual grid
-		 * in a special way
-		 */
-		constexpr auto gridlen = std::uint64_t{102};
-		constexpr auto gridsize = (gridlen * gridlen);
-
-		auto grid = std::array<std::uint64_t, gridsize>{};
-
-		const auto index = [gridlen] (auto row, auto col) {
-			return (row * gridlen + col);
-		};
-
-		// to convert a character to a number in the array
-		auto char_map = std::unordered_map<char, std::uint64_t>{
-			{'.', 0},
-			{'#', 1}
-		};
-
-		std::string line;
-
-		for(auto row = std::uint64_t{1}; std::getline(file, line); ++row) {
-
-			for(auto col : boost::counting_range({1}, (line.size() + 1))) {
-
-				grid[index(row,col)] = char_map[line[col-1]];
-
-			}
-		}
-
-		// turn the four corners on
-		constexpr auto first = std::uint64_t{1};
-		constexpr auto last = std::uint64_t{gridlen-2};
-
-		grid[index(first, first)]
-		= grid[index(first, last)]
-		= grid[index(last, first)]
-		= grid[index(last, last)] = 1;
-
-		auto tmp_grid = grid;
-
-		const auto play_conways_game_of_life = [&grid, &tmp_grid, &gridlen, &index] (auto row, auto col) {
-
-			static const auto must_turn_off = [&grid] (auto pos, auto sum) {
-				return (grid[pos] == 1) && (sum < 2 || sum > 3);
-			};
-
-			static const auto must_turn_on = [&grid] (auto pos, auto sum) {
-				return (grid[pos] == 0) && (sum == 3);
-			};
-
-			const auto sum = grid[(row-1)*gridlen+(col-1)]
-							 + grid[(row-1)*gridlen+col]
-							 + grid[(row-1)*gridlen+(col+1)]
-							 + grid[row*gridlen+(col-1)]
-							 + grid[row*gridlen+(col+1)]
-							 + grid[(row+1)*gridlen+(col-1)]
-							 + grid[(row+1)*gridlen+col]
-							 + grid[(row+1)*gridlen+(col+1)];
-
-			const auto pos = index(row, col);
-
-			if(must_turn_off(pos, sum)) {
-				tmp_grid[pos] = 0;
-			} else if(must_turn_on(pos, sum)) {
-				tmp_grid[pos] = 1;
-			}
-		};
-
-		// to work around the corners
-		constexpr auto second = (first + 1);
-
-		constexpr auto steps = std::uint64_t{100};
-
-		for(const auto step : boost::counting_range({}, steps)) {
-
-			std::uint64_t row, col;
-
-			for(row = first, col = second; col < last; ++col) {
-				play_conways_game_of_life(row, col);
-			}
-
-			for(row = second; row < last; ++ row) {
-
-				for(col = first; col <= last; ++col) {
-					play_conways_game_of_life(row, col);
-				}
-			}
-
-			for(row = last, col = second; col < last; ++col) {
-				play_conways_game_of_life(row, col);
-			}
-
-			grid = tmp_grid;
-		}
-
-		const auto count = std::accumulate(grid.begin(), grid.end(), std::uint64_t{});
-
-		std::cout << count << std::endl;
-
-	} else {
-		std::cerr << "Error! Could not open \"" << filename << "\"!" << std::endl;
-	}
-
-	return 0;
 }
