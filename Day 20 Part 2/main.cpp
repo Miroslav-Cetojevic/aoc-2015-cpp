77fbe864608c81004ddd356717d865a5f9813c20
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2020-01-21 12:21:36 +0100

    Same as Part 1 v1

--- Day 20 Part 2/main.cpp
+++ Day 20 Part 2/main.cpp
@@ -1,43 +1,48 @@
-/*
- * NOTE: this solution was taken straight from
- * https://www.reddit.com/r/adventofcode/comments/3xjpp2/day_20_solutions/
- */
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-#include <boost/range/counting_range.hpp>
+#include <boost/range/irange.hpp>
+
+auto solution(int total_presents) {
+
+  const auto total_houses = (total_presents / 11);
+  const auto final_house = 50;
+
+  /*
+   * A std::array of size 3+ million elements is too much
+   * for the stack, so std::vector is used here instead
+   */
+  auto houses = std::vector<decltype(total_presents)>(total_houses);
+
+  auto min_house = total_houses;
+
+  for(const auto elf_id : boost::irange(2, (total_houses))) {
+
+    if(houses[elf_id] + elf_id >= total_houses){
+      min_house = elf_id;
+      break;
+    }
+
+    for(const auto house_id : boost::irange(1, (final_house + 1))) {
+
+      const auto presents = elf_id * house_id;
+
+      if(presents >= total_houses) {
+        break;
+      }
+
+      houses[presents] += elf_id;
+    }
+  }
+
+  return min_house;
+}
 
 int main() {
 
-	const auto total_presents = 33100000;
-	const auto n_presents = 11;
-	const auto total_houses = (total_presents / n_presents);
-	const auto max_houses = 50;
+  const auto total_presents = 33100000;
 
-	// array with 3 mil elements will smash the stack, so vector is used instead
-	// every house has at least 11 presents because the first elf visits them all
-	auto houses = std::vector(total_houses, n_presents);
+  std::cout << solution(total_presents) << std::endl;
 
-	// elf_id is one-based, but house_id is zero-based because it's used to access the vector
-	for(const auto elf_id : boost::counting_range(2, (total_houses + 1))) {
-
-		for(auto house_id = (elf_id - 1), houses_visited = 0;
-			(houses_visited < max_houses) && (house_id < total_houses);
-			house_id += elf_id, ++houses_visited) {
-
-			houses[house_id] += (elf_id * n_presents);
-		}
-	}
-
-	const auto lowest_house = std::find_if(houses.begin(), houses.end(), [total_presents] (auto house) {
-		return (house >= total_presents);
-	});
-
-	// add 1 to account for zero-based indexing of vector
-	const auto house_number = std::distance(houses.begin(), lowest_house) + 1;
-
-	std::cout << house_number << std::endl;
-
-	return 0;
 }
